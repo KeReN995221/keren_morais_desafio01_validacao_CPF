531f567d4f4c781e960ef469c1c3fe8f7166bbaf
Keren Apuque <cardosokerenapuque@gmail.com>
2024-04-10 16:51:25 -0300

    feat: task concluida

--- src/main/java/classes/Cpf.java
+++ src/main/java/classes/Cpf.java
@@ -20,6 +20,6 @@ public class Cpf {
 
     @Override
     public String toString() {
-        return  cpf + '\'';
+        return  cpf;
     }
 }
--- src/main/java/servicos/Validacao.java
+++ src/main/java/servicos/Validacao.java
@@ -2,17 +2,19 @@ package servicos;
 
 import classes.Cpf;
 
-import java.util.List;
-
 public class Validacao {
 
     public boolean verificarQuantidade(Cpf cpfs){
-        if (cpfs.getCpf().length() == 11){
-            return true;
-        }
+
+        if (cpfs.getCpf().length() == 11){return true;}
         return false;
     }
+    public boolean verificarNumeros(Cpf cpfs){
 
-   
-
+       try{
+            long cpf = Long.parseLong(cpfs.getCpf());
+            return true;
+       }catch (RuntimeException e){System.out.println(e.getMessage());}
+       return false;
+    }
 }
