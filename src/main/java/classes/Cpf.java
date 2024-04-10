24016b646dda0325f7143d93565fff7492950f1b
Keren Apuque <cardosokerenapuque@gmail.com>
2024-04-10 11:18:24 -0300

    fix: reestruturação do projeto

--- src/main/java/classes/Cpf.java
+++ src/main/java/classes/Cpf.java
@@ -5,36 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cpf {
-    private List<String> cpf;
-    private List<String> cpfValidos = new ArrayList<>();
-    private List<String> cpfInvalidos = new ArrayList<>();
+    private String cpf;
 
     public Cpf(){
     }
-    public Cpf(List<String> cpf) {
+
+    public Cpf(String cpf){
         this.cpf = cpf;
     }
 
-    public void verificarQuantidade(List<String> cpfs){
-        for (String cpf : cpfs){
-            if (cpf.length() == 11){
-                cpfValidos.add(cpf);
-            }
-            else{
-                cpfInvalidos.add(cpf);
-            }
-        }
-
-        System.out.println("CPFs com a quantidade numérica válida: ");
-        for (String cpfV : cpfValidos) {
-
-            System.out.println(cpfV);
-        }
-        System.out.println("CPFs com a quantidade numérica inválida: ");
-        for (String cpfI : cpfInvalidos){
-            System.out.println(cpfI);
-        }
+    public String getCpf() {
+        return cpf;
     }
 
-
+    @Override
+    public String toString() {
+        return  cpf + '\'';
+    }
 }
--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -1,6 +1,7 @@
 package principal;
 
 import classes.Cpf;
+import servicos.Validacao;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,10 +12,41 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<String> list = Arrays.asList("12345678900", "987654210", "11122233344", "1111111111111111111111111111111");
 
-        Cpf cpfs = new Cpf();
-        cpfs.verificarQuantidade(list);
+        List<Cpf> cpfValidos = new ArrayList<>();
+        List<Cpf> cpfInvalidos = new ArrayList<>();
+        List<Cpf> cpfs = new ArrayList<>();
+        cpfs.add(new Cpf("11122233344"));
+        cpfs.add(new Cpf("12345678910"));
+        cpfs.add(new Cpf("55566677788"));
+        cpfs.add(new Cpf("12345678912312"));
+        cpfs.add(new Cpf("12312312"));
+        cpfs.add(new Cpf("12"));
+        cpfs.add(new Cpf("12345678944"));
+        cpfs.add(new Cpf("A12547"));
+        cpfs.add(new Cpf("A1231231232"));
+        Validacao validacao = new Validacao();
+
+        for (Cpf cpf : cpfs) {
+            if (validacao.verificarQuantidade(cpf) == true){
+                cpfValidos.add(cpf);
+
+            }
+            else{
+                cpfInvalidos.add(cpf);
+            }
+
+        }
+
+        System.out.println("CPF válidos: ");
+        for (Cpf cpf : cpfValidos){
+            System.out.println(cpf.toString());
+        }
+        System.out.println("CPF inválidos: ");
+        for (Cpf cpf : cpfInvalidos){
+            System.out.println(cpf.toString());
+        }
+
 
     }
 }
