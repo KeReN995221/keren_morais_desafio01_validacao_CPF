c5671f15885e384cfc295c8d15b962d3359e1099
Keren Apuque <cardosokerenapuque@gmail.com>
2024-04-11 09:23:38 -0300

    feat: inclusão do método modificador set

--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -16,31 +16,33 @@ public class Main {
         List<Cpf> cpfValidos = new ArrayList<>();
         List<Cpf> cpfInvalidos = new ArrayList<>();
         List<Cpf> cpfs = new ArrayList<>();
-        cpfs.add(new Cpf("11122233344"));
+        cpfs.add(new Cpf("111.222.333-44"));
         cpfs.add(new Cpf("12345678910"));
         cpfs.add(new Cpf("55566677788"));
         cpfs.add(new Cpf("12345678912312"));
-        cpfs.add(new Cpf("12312312"));
+        cpfs.add(new Cpf("123.123.12"));
         cpfs.add(new Cpf("12"));
         cpfs.add(new Cpf("12345678944BVC"));
         cpfs.add(new Cpf("A12547"));
         Validacao validacao = new Validacao();
 
         for (Cpf cpf : cpfs) {
-            if (validacao.verificarQuantidade(cpf) == true){
-                if (validacao.verificarNumeros(cpf) == true){
-                    cpfValidos.add(cpf);
+
+                if (validacao.temCaracterEspecial(cpf) == true) {
+                    if (validacao.ehApenasNum(cpf) == true) {
+                        cpfValidos.add(cpf);
+                    }
+                } else {
+                    cpfInvalidos.add(cpf);
                 }
-            }
-            else{ cpfInvalidos.add(cpf);
-            }
+
         }
 
         System.out.println("CPF válidos numericamente e pela quantidade de caracter : ");
         for (Cpf cpf : cpfValidos){
             System.out.println(cpf.toString());
         }
-        System.out.println("CPF inválidos  numericamente pela quantidade de caracter: ");
+        System.out.println("CPF inválidos numericamente e pela quantidade de caracter: ");
         for (Cpf cpf : cpfInvalidos){
             System.out.println(cpf.toString());
         }
